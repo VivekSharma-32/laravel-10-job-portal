06b5ed655bec58fe98a745b505d70c61e7f69fb1
VivekSharma-32 <vivek.sharma1275@gmail.com>
2024-03-04 19:35:22 +0530

    changes

--- resources/views/front/jobDetail.blade.php
+++ resources/views/front/jobDetail.blade.php
@@ -1,4 +1,5 @@
 @extends('front.layouts.app')
+
 @section('main')
     <section class="section-4 bg-2">
         <div class="container pt-5">
@@ -7,8 +8,7 @@
                     <nav aria-label="breadcrumb" class=" rounded-3 p-3">
                         <ol class="breadcrumb mb-0">
                             <li class="breadcrumb-item"><a href="{{ route('jobs') }}"><i class="fa fa-arrow-left"
-                                        aria-hidden="true"></i>
-                                    &nbsp;Back to Jobs</a></li>
+                                        aria-hidden="true"></i> &nbsp;Back to Jobs</a></li>
                         </ol>
                     </nav>
                 </div>
@@ -39,7 +39,7 @@
                                 </div>
                                 <div class="jobs_right">
                                     <div class="apply_now {{ $count == 1 ? 'saved-job' : '' }}">
-                                        <a class="heart_mark " href="javascript:void(0)"
+                                        <a class="heart_mark " href="javascript:void(0);"
                                             onclick="saveJob({{ $job->id }})"> <i class="fa fa-heart-o"
                                                 aria-hidden="true"></i></a>
                                     </div>
@@ -49,10 +49,10 @@
                         <div class="descript_wrap white-bg">
                             <div class="single_wrap">
                                 <h4>Job description</h4>
-                                {!! nl2br(strip_tags($job->description)) !!}
+                                {!! nl2br($job->description) !!}
+
 
                             </div>
-
                             @if (!empty($job->responsibility))
                                 <div class="single_wrap">
                                     <h4>Responsibility</h4>
@@ -73,24 +73,29 @@
                             @endif
                             <div class="border-bottom"></div>
                             <div class="pt-3 text-end">
+
                                 @if (Auth::check())
-                                    <a href="javascript:void(0)" onclick="saveJob({{ $job->id }})"
+                                    <a href="#" onclick="saveJob({{ $job->id }});"
                                         class="btn btn-secondary">Save</a>
                                 @else
-                                    <a href="javascript:void(0)" onclick="saveJob({{ $job->id }})"
-                                        href="javascript:void()" class="btn btn-secondary disabled">Save</a>
+                                    <a href="javascript:void(0);" class="btn btn-secondary disabled">Login to Save</a>
                                 @endif
+
                                 @if (Auth::check())
                                     <a href="#" onclick="applyJob({{ $job->id }})"
                                         class="btn btn-primary">Apply</a>
                                 @else
-                                    <a href="javascript:void()" class="btn btn-primary disabled">Login To Apply</a>
+                                    <a href="javascript:void(0);" class="btn btn-primary disabled">Login to Apply</a>
                                 @endif
+
+
                             </div>
                         </div>
                     </div>
+
                     @if (Auth::user())
                         @if (Auth::user()->id == $job->user_id)
+
 
 
                             <div class="card shadow border-0 mt-4">
@@ -101,8 +106,7 @@
                                                 <h4>Applicants</h4>
                                             </div>
                                         </div>
-                                        <div class="jobs_right">
-                                        </div>
+                                        <div class="jobs_right"></div>
                                     </div>
                                 </div>
                                 <div class="descript_wrap white-bg">
@@ -119,19 +123,19 @@
                                                     <td>{{ $application->user->name }}</td>
                                                     <td>{{ $application->user->email }}</td>
                                                     <td>{{ $application->user->mobile }}</td>
-                                                    <td>{{ \Carbon\Carbon::parse($application->applied_date)->format('d M,Y') }}
+                                                    <td>
+                                                        {{ \Carbon\Carbon::parse($application->applied_date)->format('d M, Y') }}
                                                     </td>
                                                 </tr>
                                             @endforeach
                                         @else
                                             <tr>
-                                                <td colspan="4">
-                                                    <p class="mb-0 py-3 text-center">Job Applicants Not Found</p>
-                                                </td>
+                                                <td colspan="3">Applicants not found</td>
                                             </tr>
                                         @endif
 
                                     </table>
+
                                 </div>
                             </div>
                         @endif
@@ -146,13 +150,14 @@
                             <div class="job_content pt-3">
                                 <ul>
                                     <li>Published on:
-                                        <span>{{ \Carbon\Carbon::parse($job->created_at)->format('d M, Y') }}</span>
-                                    </li>
+                                        <span>{{ \Carbon\Carbon::parse($job->created_at)->format('d M, Y') }}</span></li>
                                     <li>Vacancy: <span>{{ $job->vacancy }}</span></li>
+
 
                                     @if (!empty($job->salary))
                                         <li>Salary: <span>{{ $job->salary }}</span></li>
                                     @endif
+
                                     <li>Location: <span>{{ $job->location }}</span></li>
                                     <li>Job Nature: <span> {{ $job->jobType->name }}</span></li>
                                 </ul>
@@ -167,14 +172,17 @@
                             <div class="job_content pt-3">
                                 <ul>
                                     <li>Name: <span>{{ $job->company_name }}</span></li>
+
                                     @if (!empty($job->company_location))
                                         <li>Locaion: <span>{{ $job->company_location }}</span></li>
                                     @endif
+
                                     @if (!empty($job->company_website))
-                                        <li>Webite: <span><a href="{{ $job->company_website }}"
-                                                    target="_blank">{{ $job->company_website }}</a></span>
+                                        <li>Webite: <span><a
+                                                    href="{{ $job->company_website }}">{{ $job->company_website }}</a></span>
                                         </li>
                                     @endif
+
                                 </ul>
                             </div>
                         </div>
@@ -183,6 +191,7 @@
             </div>
         </div>
     </section>
+
 @endsection
 
 @section('customJs')
@@ -190,7 +199,7 @@
         function applyJob(id) {
             if (confirm("Are you sure you want to apply on this job?")) {
                 $.ajax({
-                    url: "{{ route('applyJob') }}",
+                    url: '{{ route('applyJob') }}',
                     type: 'post',
                     data: {
                         id: id
@@ -205,7 +214,7 @@
 
         function saveJob(id) {
             $.ajax({
-                url: "{{ route('saveJob') }}",
+                url: '{{ route('saveJob') }}',
                 type: 'post',
                 data: {
                     id: id
